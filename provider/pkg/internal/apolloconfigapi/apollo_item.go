2a33c7682d94b0ca92f2ce515beeba6d102d533d
tangshoubin <tangshoubin@itiger.com>
2024-11-14 21:15:00 +0800

    update

--- provider/pkg/internal/apolloconfigapi/apollo_item.go
+++ provider/pkg/internal/apolloconfigapi/apollo_item.go
@@ -25,7 +25,8 @@ import (
 type ApolloItemClient interface {
 	CreateApolloItem(ctx context.Context, params *CreateUpdateApollItemRequest) (*ApollItem, error)
 	UpdateApolloItem(ctx context.Context, apolloItemId, orgName, name, description string) error
-	DeleteApolloItem(ctx context.Context, apolloItemId, orgName string, forceDestroy bool) error
+	DeleteApolloItem(ctx context.Context, env, appId, clusterName, namespace, key, operator string) error
+	// DeleteApolloItem(ctx context.Context, apolloItemId, orgName string, forceDestroy bool) error
 	GetApolloItem(ctx context.Context, apolloItemId, orgName string) (*ApollItem, error)
 }
 
@@ -34,11 +35,28 @@ type ApollItem struct {
 	Name        string `json:"name"`
 	Description string `json:"description,omitempty"`
 	TokenValue  string `json:"tokenValue"`
+
+	Env 			     string `json:"env"`
+	AppId                string `json:"appId"`
+	ClusterName          string `json:"clusterName"`
+	Namespace            string `json:"namespace"`
+	Key                  string `json:"key"`
+	Value                string `json:"value"`
+	Comment              string `json:"comment"`
+	DataChangeCreatedBy  string `json:"dataChangeCreatedBy"`
+
+	DataChangeLastModifiedBy    string `json:"dataChangeLastModifiedBy"`
+	DataChangeCreatedTime       string `json:"dataChangeCreatedTime"`
+	DataChangeLastModifiedTime  string `json:"dataChangeLastModifiedTime"`
 }
 
 type createApollItemResponse struct {
 	ID         string `json:"id"`
 	TokenValue string `json:"tokenValue"`
+
+	DataChangeLastModifiedBy    string `json:"dataChangeLastModifiedBy"`
+	DataChangeCreatedTime       string `json:"dataChangeCreatedTime"`
+	DataChangeLastModifiedTime  string `json:"dataChangeLastModifiedTime"`
 }
 
 type CreateUpdateApollItemRequest struct {
@@ -47,6 +65,14 @@ type CreateUpdateApollItemRequest struct {
     Env                       string `json:"env"`
     ClusterName               string `json:"clusterName"`
     DataChangeLastModifiedBy  string `json:"dataChangeLastModifiedBy"`
+	Key                       string `json:"key"`
+	Value                     string `json:"value"`
+	Comment                   string `json:"comment"`
+	Operator                  string `json:"operator"`
+	DataChangeCreatedBy       string `json:"dataChangeCreatedBy"`
+}
+
+type CreateUpdateApollItemPostData struct {
 	Key                       string `json:"key"`
 	Value                     string `json:"value"`
 	Comment                   string `json:"comment"`
@@ -76,7 +102,7 @@ func (c *Client) CreateApolloItem(ctx context.Context, params *CreateUpdateApoll
 
 	apiPath := path.Join("v1", "envs", params.Env, "apps", params.AppID, "clusters", params.ClusterName, "namespaces", params.Namespace, "items")
 
-	createReq := CreateUpdateApollItemRequest{
+	createReq := CreateUpdateApollItemPostData{
 		Key:                 params.Key,
 		Value:               params.Value,
 		Comment:             params.Comment,
@@ -101,6 +127,19 @@ func (c *Client) CreateApolloItem(ctx context.Context, params *CreateUpdateApoll
 		Name:        createReq.Key,
 		Description: createReq.Comment,
 		TokenValue:  createRes.TokenValue,
+
+		Env: 			      params.Env,
+		AppId:                params.AppID,
+		ClusterName:          params.ClusterName,
+		Namespace:            params.Namespace,
+		Key:                  params.Key,
+		Value:                params.Value,
+		Comment:              params.Comment,
+		DataChangeCreatedBy:  params.DataChangeCreatedBy,
+	
+		DataChangeLastModifiedBy:    createRes.DataChangeLastModifiedBy,
+		DataChangeCreatedTime:       createRes.DataChangeCreatedTime,
+		DataChangeLastModifiedTime:  createRes.DataChangeLastModifiedTime,
 	}, nil
 
 }
@@ -134,25 +173,52 @@ func (c *Client) UpdateApolloItem(ctx context.Context, apolloItemId, orgName, na
 	return nil
 }
 
-func (c *Client) DeleteApolloItem(ctx context.Context, apolloItemId, orgName string, forceDestroy bool) error {
-	if len(apolloItemId) == 0 {
-		return errors.New("apolloItemId length must be greater than zero")
-	}
+// func (c *Client) DeleteApolloItem(ctx context.Context, apolloItemId, orgName string, forceDestroy bool) error {
+// 	if len(apolloItemId) == 0 {
+// 		return errors.New("apolloItemId length must be greater than zero")
+// 	}
 
-	if len(orgName) == 0 {
-		return errors.New("orgName length must be greater than zero")
-	}
+// 	if len(orgName) == 0 {
+// 		return errors.New("orgName length must be greater than zero")
+// 	}
 
-	apiPath := path.Join("orgs", orgName, "apollo-items", apolloItemId)
+// 	apiPath := path.Join("orgs", orgName, "apollo-items", apolloItemId)
+
+// 	var err error
+// 	if forceDestroy {
+// 		_, err = c.doWithQuery(ctx, http.MethodDelete, apiPath, url.Values{"force": []string{"true"}}, nil, nil)
+// 	} else {
+// 		_, err = c.do(ctx, http.MethodDelete, apiPath, nil, nil)
+// 	}
+// 	if err != nil {
+// 		return fmt.Errorf("failed to delete apollo item %q: %w", apolloItemId, err)
+// 	}
+
+// 	return nil
+// }
+
+func (c *Client) DeleteApolloItem(ctx context.Context, env, appId, clusterName, namespace, key, operator string) error {
+	switch {
+		case env == "":
+			return errors.New("env length must be greater than zero")
+		case appId == "":
+			return errors.New("appId length must be greater than zero")
+		case clusterName == "":
+			return errors.New("clusterName length must be greater than zero")
+		case namespace == "":
+			return errors.New("namespace length must be greater than zero")
+		case key == "":
+			return errors.New("key length must be greater than zero")
+    }
+
+	// apiPath := path.Join("orgs", orgName, "apollo-items", apolloItemId)
+	apiPath := path.Join("v1", "envs", env, "apps", appId, "clusters", clusterName, "namespaces", namespace, "items", key)
+
 
 	var err error
-	if forceDestroy {
-		_, err = c.doWithQuery(ctx, http.MethodDelete, apiPath, url.Values{"force": []string{"true"}}, nil, nil)
-	} else {
-		_, err = c.do(ctx, http.MethodDelete, apiPath, nil, nil)
-	}
+	_, err = c.doWithQuery(ctx, http.MethodDelete, apiPath, url.Values{"operator": []string{operator}}, nil, nil)
 	if err != nil {
-		return fmt.Errorf("failed to delete apollo item %q: %w", apolloItemId, err)
+		return fmt.Errorf("failed to delete apollo item %q: %w", key, err)
 	}
 
 	return nil
